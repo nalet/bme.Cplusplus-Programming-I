58207cb63c163c6970cd7a916a2e3c9e67a62efc
Nalet Meinen <nalet@adc.certe.ch>
2019-03-05 19:22:32 +0100

    cosmetics

--- Ex01/main.cpp
+++ Ex01/main.cpp
@@ -12,15 +12,14 @@ int main()
     double ratio = 1;
     double const dev_template = 1.61803399;
 
-    std :: cout << fprev << std :: endl ;
+    std :: cout << std::fixed << std::setprecision(17) << fprev << std :: endl ;
 
      do {
-     std :: cout << f << "\t\t Ratio: " << std::fixed << std::setprecision(17) << ratio << " - Dev[%]: " << ( ratio - dev_template ) / dev_template * -100 << std :: endl ;
-     int tmp = fibonacci (f, fprev );
-     ratio = goldenRatio(f, fprev);
-     fprev = f;
-     f = tmp ;
-
+         std :: cout << f << "\t\t Ratio: " << ratio << " - Dev[%]: " << ( ratio - dev_template ) / dev_template * -100 << std :: endl ;
+         int tmp = fibonacci (f, fprev );
+         ratio = goldenRatio(f, fprev);
+         fprev = f;
+         f = tmp ;
      } while ( fprev <= f );
      std :: cout << std :: endl ;
      std :: cout << "Max int value: " << std::numeric_limits<int>::max() << std :: endl ;
